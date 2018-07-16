4e33b8094c45f966678e0f13165424c629af48f2
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-07-16 12:00:53 +0200

    First aproximation to redux

---
+++ app.ts
@@ -0,0 +1,57 @@
+// Acciones
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+// const incrementadorAction: Action = {
+//     type: 'INCREMENTAR'
+// };
+
+// Reducer
+
+/* Reducer siempre regresan un state */
+
+function reducer( state = 10, action: Action) {
+
+    switch( action.type ){
+        case 'INCREMENTAR':
+            return state += 1;
+        case 'DECREMENTAR':
+            return state -= 1;
+        case 'MULTIPLICAR':
+            return state * action.payload;
+        case 'DIVIDIR':
+            return state / action.payload;
+        default:
+            return state; 
+    }
+}
+
+// Usar Reducer
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+
+ console.log( reducer(10, incrementadorAction) );
+
+ const decrementadorAction: Action = {
+    type: 'DECREMENTAR'
+};
+
+ console.log( reducer(10, decrementadorAction) );
+
+ const multiplicadorAction: Action = {
+     type: 'MULTIPLICAR',
+     payload: 2
+ }
+
+ console.log( reducer(10, multiplicadorAction) );
+
+ const divididorAction: Action = {
+    type: 'DIVIDIR',
+    payload: 2
+}
+
+console.log( reducer(10, divididorAction) );
